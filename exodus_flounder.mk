ccf31f7a45fa036678f1588c28a6db99e8f6b283
Dave Kessler <activethrasher00@gmail.com>
2015-06-14 15:55:05 -0400

    add exodus vendor setup stuffs
    Change-Id: I8a4c504c160989354ecfc4e9f47d6eb4c091dc53

---
+++ exodus_flounder.mk
@@ -0,0 +1,50 @@
+# CyanogenMod Specific Changes
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2048
+TARGET_SCREEN_WIDTH := 1536
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/exodus/config/common_full_tablet_wifionly.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/exodus/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/flounder/aosp_flounder.mk)
+
+$(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
+
+# Inline kernel building
+TARGET_PREBUILT_KERNEL := device/htc/flounder-kernel/kernel
+#KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+#KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+#TARGET_KERNEL_SOURCE := kernel/htc/flounder
+#TARGET_KERNEL_CONFIG := flounder_defconfig
+#BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
+
+ifneq ($(TARGET_PREBUILT_KERNEL),)
+  ifneq ("$(wildcard $(TARGET_PREBUILT_KERNEL))","")
+    PRODUCT_COPY_FILES += \
+      $(TARGET_PREBUILT_KERNEL):$(ANDROID_BUILD_TOP)/out/target/product/$(TARGET_DEVICE)/kernel
+  else
+    $(error no prebuilt kernel exists on path $(TARGET_PREBUILT_KERNEL)!!)
+  endif
+endif
+
+# Extra Packages
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras
+
+# Copy Extra Files
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
+
+# CM Overlays
+DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := exodus_flounder
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 9
