3e6fcb0f72ddf5a3b6977ddfc7c34551b56f4b75
Dave Kessler <activethrasher00@gmail.com>
2016-02-22 18:39:26 -0500

    disable verity warnings
    Change-Id: Iaef13f308992678e64ea3f62ad2fe736dffd1b71

--- verity/Android.mk
+++ verity/Android.mk
@@ -25,28 +25,28 @@
 
 LOCAL_PATH := $(call my-dir)
 
-define _add-warning-image
-include $$(CLEAR_VARS)
-LOCAL_MODULE := warning_$(notdir $(1))
-LOCAL_MODULE_STEM := $(notdir $(1))
-_img_modules += $$(LOCAL_MODULE)
-LOCAL_SRC_FILES := $1
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $$(TARGET_ROOT_OUT)/res/images/warning
-include $$(BUILD_PREBUILT)
-endef
+#define _add-warning-image
+#include $$(CLEAR_VARS)
+#LOCAL_MODULE := warning_$(notdir $(1))
+#LOCAL_MODULE_STEM := $(notdir $(1))
+#_img_modules += $$(LOCAL_MODULE)
+#LOCAL_SRC_FILES := $1
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_MODULE_PATH := $$(TARGET_ROOT_OUT)/res/images/warning
+#include $$(BUILD_PREBUILT)
+#endef
 
-_img_modules :=
-_images :=
-$(foreach _img, $(call find-subdir-subdir-files, "images", "*.png"), \
-  $(eval $(call _add-warning-image,$(_img))))
+#_img_modules :=
+#_images :=
+#$(foreach _img, $(call find-subdir-subdir-files, "images", "*.png"), \
+#  $(eval $(call _add-warning-image,$(_img))))
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := verity_warning_images
-LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(_img_modules)
-include $(BUILD_PHONY_PACKAGE)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := verity_warning_images
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_REQUIRED_MODULES := $(_img_modules)
+#include $(BUILD_PHONY_PACKAGE)
 
-_add-warning-image :=
-_img_modules :=
+#_add-warning-image :=
+#_img_modules :=
